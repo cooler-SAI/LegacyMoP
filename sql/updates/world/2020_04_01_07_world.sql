ed6875e477c79a65a0b8ed35dab0ecc274440bff
zgn <gennady.zamalaev@gmail.com>
2020-04-01 20:35:49 +0300

    Spell/Druid: Teleport : Moonglade, Faerie Fire

---
+++ sql/updates/world/2020_04_01_07_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (18960, 770);
+INSERT INTO `spell_script_names` VALUE 
+(18960, 'spell_druid_teleport_moonglade'),
+(770, 'spell_druid_faerie_fire');
