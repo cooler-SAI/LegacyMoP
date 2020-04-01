3ae8a499ddf393460c81dffa7deb3b991366f0b4
zgn <gennady.zamalaev@gmail.com>
2020-04-01 22:10:58 +0300

    Spell/Druid: Celestial Alignment, Astral Communion

---
+++ sql/updates/world/2020_04_01_09_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (112071, 127663);
+INSERT INTO `spell_script_names` VALUE 
+(112071, 'spell_druid_celestial_alignment'),
+(127663, 'spell_druid_astral_communion');
