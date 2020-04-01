79dcc59cf067043aeba0a06ddc27ddda8cd33a9d
zgn <gennady.zamalaev@gmail.com>
2020-04-01 18:19:53 +0300

    добавляем Пик Тигра арену

---
+++ src/server/game/Battlegrounds/Zones/BattlegroundTTP.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Battleground.h"
+#include "BattlegroundTTP.h"
+#include "Language.h"
+#include "Object.h"
+#include "ObjectMgr.h"
+#include "Player.h"
+#include "WorldPacket.h"
+
+BattlegroundTTP::BattlegroundTTP()
+{
+    BgObjects.resize(BG_TTP_OBJECT_MAX);
+
+    StartDelayTimes[BG_STARTING_EVENT_FIRST]  = BG_START_DELAY_1M;
+    StartDelayTimes[BG_STARTING_EVENT_SECOND] = BG_START_DELAY_30S;
+    StartDelayTimes[BG_STARTING_EVENT_THIRD]  = BG_START_DELAY_15S;
+    StartDelayTimes[BG_STARTING_EVENT_FOURTH] = BG_START_DELAY_NONE;
+    //we must set messageIds
+    StartMessageIds[BG_STARTING_EVENT_FIRST]  = LANG_ARENA_ONE_MINUTE;
+    StartMessageIds[BG_STARTING_EVENT_SECOND] = LANG_ARENA_THIRTY_SECONDS;
+    StartMessageIds[BG_STARTING_EVENT_THIRD]  = LANG_ARENA_FIFTEEN_SECONDS;
+    StartMessageIds[BG_STARTING_EVENT_FOURTH] = LANG_ARENA_HAS_BEGUN;
+}
+
+BattlegroundTTP::~BattlegroundTTP()
+{
+
+}
+
+void BattlegroundTTP::StartingEventCloseDoors()
+{
+    for (uint32 i = BG_TTP_OBJECT_DOOR_1; i <= BG_TTP_OBJECT_DOOR_2; ++i)
+        SpawnBGObject(i, RESPAWN_IMMEDIATELY);
+}
+
+void BattlegroundTTP::StartingEventOpenDoors()
+{
+    for (uint32 i = BG_TTP_OBJECT_DOOR_1; i <= BG_TTP_OBJECT_DOOR_2; ++i)
+        DoorOpen(i);
+}
+
+void BattlegroundTTP::AddPlayer(Player* player)
+{
+    Battleground::AddPlayer(player);
+    //create score and add it to map, default values are set in constructor
+    BattlegroundTTPScore* sc = new BattlegroundTTPScore;
+
+    PlayerScores[player->GetGUID()] = sc;
+
+    UpdateArenaWorldState();
+}
+
+void BattlegroundTTP::RemovePlayer(Player* /*player*/, uint64 /*guid*/, uint32 /*team*/)
+{
+    if (GetStatus() == STATUS_WAIT_LEAVE)
+        return;
+
+    UpdateArenaWorldState();
+    CheckArenaWinConditions();
+}
+
+void BattlegroundTTP::HandleKillPlayer(Player* player, Player* killer)
+{
+    if (GetStatus() != STATUS_IN_PROGRESS)
+        return;
+
+    if (!killer)
+    {
+        sLog->outError(LOG_FILTER_BATTLEGROUND, "BattlegroundTTP: Killer player not found");
+        return;
+    }
+
+    Battleground::HandleKillPlayer(player, killer);
+
+    UpdateArenaWorldState();
+    CheckArenaWinConditions();
+}
+
+bool BattlegroundTTP::HandlePlayerUnderMap(Player* player)
+{
+    player->TeleportTo(GetMapId(), 4055.504395f, 2919.660645f, 13.611241f, player->GetOrientation(), false);
+    return true;
+}
+
+void BattlegroundTTP::HandleAreaTrigger(Player* Source, uint32 Trigger)
+{
+    if (GetStatus() != STATUS_IN_PROGRESS)
+        return;
+
+    //uint32 SpellId = 0;
+    //uint64 buff_guid = 0;
+    switch (Trigger)
+    {
+        case 9127:                                          // start position
+        case 9126:                                          // start position
+            break;
+        default:
+            sLog->outError(LOG_FILTER_BATTLEGROUND, "WARNING: Unhandled AreaTrigger in Battleground: %u", Trigger);
+            Source->GetSession()->SendAreaTriggerMessage("Warning: Unhandled AreaTrigger in Battleground: %u", Trigger);
+            break;
+    }
+
+    //if (buff_guid)
+    //    HandleTriggerBuff(buff_guid, Source);
+}
+
+void BattlegroundTTP::FillInitialWorldStates(WorldStateBuilder& builder)
+{
+    builder.AppendState(0xa11, 1);
+    UpdateArenaWorldState();
+}
+
+void BattlegroundTTP::Reset()
+{
+    //call parent's class reset
+    Battleground::Reset();
+}
+
+bool BattlegroundTTP::SetupBattleground()
+{
+    // gates
+    if (!AddObject(BG_TTP_OBJECT_DOOR_1, BG_TTP_OBJECT_TYPE_DOOR_1, 501.932f, 633.429f, 380.708f, 0.0262353f, 0, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_TTP_OBJECT_DOOR_2, BG_TTP_OBJECT_TYPE_DOOR_2, 632.101f, 633.791f, 380.704f, 3.20989f, 0, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // buffs
+        || !AddObject(BG_TTP_OBJECT_BUFF_1, BG_TTP_OBJECT_TYPE_BUFF_1, 566.6805f, 602.2274f, 383.6813f, 3.316144f, 0, 0, -1.f, 0, 120)
+        || !AddObject(BG_TTP_OBJECT_BUFF_2, BG_TTP_OBJECT_TYPE_BUFF_2, 566.6563f, 664.566f, 383.6809f, 2.460913f, 0, 0, 0, 1.f, 120))
+    {
+        SF_LOG_ERROR("sql.sql", "BattlegroundTTP: Failed to spawn some object!");
+        return false;
+    }
+
+    return true;
+}
---
+++ src/server/game/Battlegrounds/Zones/BattlegroundTTP.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __BATTLEGROUNDTTP_H
+#define __BATTLEGROUNDTTP_H
+
+#include "Battleground.h"
+
+enum BattlegroundTTPObjectTypes
+{
+    BG_TTP_OBJECT_DOOR_1         = 0,
+    BG_TTP_OBJECT_DOOR_2         = 1,
+    BG_TTP_OBJECT_BUFF_1         = 2,
+    BG_TTP_OBJECT_BUFF_2         = 3,
+    BG_TTP_OBJECT_PORT_1         = 4,
+    BG_TTP_OBJECT_PORT_2         = 5,
+    BG_TTP_OBJECT_MAX            = 6
+};
+
+enum BattlegroundTTPObjects
+{
+    BG_TTP_OBJECT_TYPE_BUFF_1     = 184664,
+    BG_TTP_OBJECT_TYPE_BUFF_2     = 184663,
+    BG_TTP_OBJECT_TYPE_DOOR_1     = 210866,     //HACK. ToDo: finde correct entry and positions
+    BG_TTP_OBJECT_TYPE_DOOR_2     = 210866,     //HACK.
+    BG_TTP_OBJECT_TYPE_PORTAL     = 181621
+};
+
+class BattlegroundTTPScore : public BattlegroundScore
+{
+    public:
+        BattlegroundTTPScore() {};
+        virtual ~BattlegroundTTPScore() {};
+        //TODO fix me
+};
+
+class BattlegroundTTP : public Battleground
+{
+    public:
+        BattlegroundTTP();
+        ~BattlegroundTTP();
+
+        /* inherited from BattlegroundClass */
+        virtual void AddPlayer(Player* player);
+        virtual void StartingEventCloseDoors();
+        virtual void StartingEventOpenDoors();
+
+        void RemovePlayer(Player* player, uint64 guid, uint32 team);
+        void HandleAreaTrigger(Player* Source, uint32 Trigger);
+        bool SetupBattleground();
+        virtual void Reset();
+        virtual void FillInitialWorldStates(WorldPacket &d);
+        void HandleKillPlayer(Player* player, Player* killer);
+        bool HandlePlayerUnderMap(Player* player);
+};
+#endif
